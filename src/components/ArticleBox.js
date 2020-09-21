b258ae003c871640dfedcc8d77de760496efe438
Ramona Liliana Paraschiv <ramonaliliana2015@yahoo.com>
2020-09-21 11:11:31 +0300

    Add article intro

--- src/components/ArticleBox.js
+++ src/components/ArticleBox.js
@@ -4,7 +4,13 @@ class ArticleBox extends Component {
     render() {
         return (
             <>
-                <p>I am the mini article</p>
+                <p>Lorem ipsum ayn rand amet</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Tincidunt risus porta quis amet sem senectus etiam lectus nisi. 
+                    Non risus feugiat nec nunc. Nibh tincidunt cursus porttitor scelerisque quisque. 
+                    Vulputate e ros, v enenatis ullamcorper urna risus nec. 
+                    Nunc, nec convallis tellus lorem commodo pretium dictumst diam, felis.
+                </p>
             </>
         )
     }
