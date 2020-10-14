f7dba7900f5b24388c63cd7e30b07dd1e58078e4
Ramona Paraschiv <ramonaliliana2015@yahoo.com>
2020-10-14 15:33:44 +0300

    implement BEM class naming

--- src/components/ArticleBox.js
+++ src/components/ArticleBox.js
@@ -5,25 +5,25 @@ class ArticleBox extends Component {
     render() {
         const categories = this.props.categories;
         return (
-            <div className="mini-article-container">
-            <div className="mini-article-container-flex">
-                <div className="mini-article-content">
+            <div className="mini-article">
+            <div className="mini-article--flex">
+                <div className="mini-article--flex-column">
                     <div>
-                        <p className="mini-article-title">{this.props.title}</p>
-                    <div className="mini-article-description">
+                        <p className="mini-article__title">{this.props.title}</p>
+                    <div className="mini-article__description">
                         <p>{this.props.description}</p>
                     </div>
-                    <hr className="mini-article-separator"/>
-                    <div className="mini-article-date-and-time">
-                        <p className="mini-article-date">{this.props.date}</p>
-                        <p className="mini-article-read-time">{this.props.time}</p>
+                    <hr className="mini-article__separator"/>
+                    <div className="mini-article__date-time date-time">
+                        <p className="date-time__date">{this.props.date}</p>
+                        <p className="date-time__time">{this.props.time}</p>
                     </div>
                     </div>
                 </div>
-                    <img className="mini-article-image" src={this.props.image} alt="Man walking on stairs" />
+                    <img className="mini-article__image" src={this.props.image} alt="Man walking on stairs" />
             </div>
-                    <div className="mini-article-categories-container">
-                        <p className="mini-article-categories">
+                    <div className="mini-article__categories">
+                        <p className="categories__category">
                             <p>Categories:</p>
                             {categories.map(category => {
                                 return <p>{category}</p>;
